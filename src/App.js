a50484ee467f015a00e8b09438eb5fff3fed2555
Abdulwahab Bakare <akoredebakare4u@gmail.com>
2023-02-01 21:01:16 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import Header from "./Header";
-import Body from "./Body";
-import Foot from "./Footer";
+import React from "react";
+// import Header from "./Header";
+// import Body from "./Body";
+// import Foot from "./Footer";
 
 const App = () => {
   return (
-    <div className="container">
-      <Header />
-      <Body />
-      <Foot />
-    </div>
+    // <div className="container">
+    //   <Header />
+    //   <Body />
+    //   <Foot />
+
+    // </div>
+    <h1>App Component</h1>
   );
 };
 export default App;
