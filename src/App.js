775e3f343f2e5c010562998110928d10bc54fd55
Abdulwahab Bakare <akoredebakare4u@gmail.com>
2023-02-01 21:28:00 -0400

    Reverted back to original

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
-// import Header from "./Header";
-// import Body from "./Body";
-// import Foot from "./Footer";
+import Header from "./Header";
+import Body from "./Body";
+import Foot from "./Footer";
 
 const App = () => {
   return (
-    // <div className="container">
-    //   <Header />
-    //   <Body />
-    //   <Foot />
+    <div className="container">
+      <Header />
+      <Body />
+      <Foot />
 
-    // </div>
-    <h1>App Component</h1>
+    </div>
   );
 };
 export default App;
